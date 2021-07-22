2823350b41c01641717578c364dec0dc3fd8be94
SeungWon <dlatmddnjs20@gmail.com>
2021-07-22 23:03:07 +0900

    heroku connect

--- config/settings.py
+++ config/settings.py
@@ -126,5 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
